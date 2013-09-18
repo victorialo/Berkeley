10c23afd0a2c9391aa81cd0d93d4c9ee91769990
Victoria <victoria@ubuntu.ubuntu-domain>
2013-09-17 23:00:11 -0700

    fixed and submitted

--- CS61B/hw1/HW1Test.java
+++ CS61B/hw1/HW1Test.java
@@ -1,6 +1,7 @@
 /** java HW1Test should test the methods defined in Progs.
  *  @author Victoria Lo
  */
+
 public class HW1Test {
 
     /** Run all tests. */
@@ -12,7 +13,7 @@ public class HW1Test {
         report("dsublist", testDsublist());
     }
 
-    /** Print message that test NAME has (if ISOK) or else has not
+    /** Print message that test NAME has(if ISOK) or else has not
      *  passed. */
     private static void report(String name, boolean isOK) {
         if (isOK) {
@@ -22,14 +23,12 @@ public class HW1Test {
         }
     }
 
-    // Replace the bodies of the functions below with something serious.
-
     /** Return true iff factorSum passes its tests. */
     private static boolean testFactorSum() {
-        if (Progs.factorSum(10) == 1 + 2 + 5){
+        if (Progs.factorSum(10) == 1 + 2 + 5) {
             if (Progs.factorSum(12) == 1 + 2 + 3 + 4 + 6) {
-                if (Progs.factorSum(220) == 284){
-                    if (Progs.factorSum(284) == 220){
+                if (Progs.factorSum(220) == 284) {
+                    if (Progs.factorSum(284) == 220) {
                         return true;
                     }
                 }
@@ -46,23 +45,31 @@ public class HW1Test {
 
     /** Return true iff dcantenate passes its tests. */
     private static boolean testDcatenate() {
-        IntList a = IntList.list (1, 3, 4);
-        IntList b = IntList.list (2, 5, 8);
-        if (Progs.dcatenate (a, b) == IntList.list (1, 3, 4, 2, 5, 8)){
+        IntList a = IntList.list(1, 3, 4);
+        IntList b = IntList.list(2, 5, 8);
+        if (Progs.dcatenate(a, b).equals(IntList.list(1, 3, 4, 2, 5, 8))) {
             return true;
         }
-        else {
-            return false;
-        }
-     }
+        return false;
+    }
     /** Return true iff sublist passes its tests. */
     private static boolean testSublist() {
-        return true;
+        IntList a = IntList.list(1, 3, 4, 2, 5, 8);
+        IntList b = Progs.sublist(a, 3, 3);
+        if (b.equals(IntList.list(4, 2, 5))) {
+            return true;
+        }
+        return false;
     }
 
     /** Return true iff dsublist passes its tests. */
     private static boolean testDsublist() {
-        return true;
+        IntList a = IntList.list(1, 3, 4, 2, 5, 8);
+        if (Progs.dsublist(a, 3, 3).equals(IntList.list(4, 2, 5))) {
+            return true;
+        }
+        return false;
     }
+
 
 }
--- CS61B/hw1/Progs.java
+++ CS61B/hw1/Progs.java
@@ -1,9 +1,8 @@
-import java.lang.Integer;
-
 /** HW #1 solutions.
  *
  *  @author Victoria Lo
  */
+
 class Progs {
 
     /* 1a. */
@@ -16,9 +15,9 @@ class Progs {
         while (k < N) {
             if (N % k == 0) {
                 total = total + k;
-                k += 1;
             }
-        } 
+            k += 1;
+        }
         return total;
     }
 
@@ -28,14 +27,15 @@ class Progs {
      *  line, smallest member of each pair first, with no repetitions). */
     static void printSociablePairs(int N) {
         /* *Fill in here* */
-        for (int k=1; k < N; k++) {
-            for (int m=2; m < N; m++) {
+        for (int k = 1; k < N; k += 1) {
+            for (int m = 2; m < N; m += 1) {
                 if (k == factorSum(m) && (m == factorSum(k))) {
                     if (k < m) {
-                        System.out.println(Integer.toString(k) + Integer.toString(m));
-                    }
-                    else {
-                        System.out.println(Integer.toString(m) + Integer.toString(k));
+                        System.out.println(Integer.toString(k)
+                            + Integer.toString(m));
+                    } else {
+                        System.out.println(Integer.toString(m)
+                            + Integer.toString(k));
                     }
                 }
             }
@@ -47,10 +47,11 @@ class Progs {
      *  elements of B.  May modify items of A. Don't use 'new'. */
     static IntList dcatenate(IntList A, IntList B) {
         /* *Replace the following with the answer* */
-        while (A != null){
-            A = A.tail;
+        IntList C = A.tail;
+        while (C.tail != null) {
+            C = C.tail;
         }
-        A.tail = B;
+        C.tail = B;
         return A;
     }
 
@@ -61,20 +62,20 @@ class Progs {
      *  It is an error i the desired items don't exist. */
     static IntList sublist(IntList L, int start, int len) {
         /* *Replace the following with the answer* */
-        int i = 0;
         IntList m = null;
-        while (L != null){
-            if (i < start) {
-                L = L.tail;
-                i ++; 
-            }
-            else {
-                for (i = start; i < len; i++) {
-                   m.tail = L;
-                }
-            }
+        for (int i = 0; i < start - 1; i += 1) {
+            L = L.tail;
         }
-        return m;
+        m = L;
+        IntList result = new IntList(m.head, null);
+        IntList holder = result;
+        IntList end = m.tail;
+        for (int i = start; i < (len + start) - 1; i += 1) {
+            holder.tail = new IntList(end.head, null);
+            holder = holder.tail;
+            end = end.tail;
+        }
+        return result;
     }
 
     /* 2c. */
@@ -84,7 +85,20 @@ class Progs {
      *  It is an error if the desired items don't exist. */
     static IntList dsublist(IntList L, int start, int len) {
         /* *Replace the following with the answer* */
-        return null;
+        IntList m = null;
+        for (int i = 0; i < start - 1; i += 1) {
+            L = L.tail;
+        }
+        m = L;
+        IntList holder = m;
+        IntList end = m.tail;
+        for (int i = start; i < (len + start) - 1; i += 1) {
+            holder.tail = end;
+            holder = holder.tail;
+            end = end.tail;
+        }
+        holder.tail = null;
+        return m;
     }
 
 }
